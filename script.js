c1c4451ff66d704831023e9f58700608a784e2e8
Feleks_Webs <49903400+FeleksWebs@users.noreply.github.com>
2020-01-11 15:12:57 +0000

    Making better adjustments for bugs

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+////////////   On Click Random Number   ////////////
 var Timeout;
 var num = 0
 
@@ -8,40 +11,28 @@ function getRandomInt(min, max) {
 }
 
 function onClick(){
+if(lowNr != '' && highNr != ''){
   var lowNr = document.getElementById('input1').value
   var highNr = document.getElementById('input2').value
   var txtNr = document.getElementById('Random_Dice')
-
-
-    Timeout = setInterval(() => {
-      num++
-      if(num > 9){
-        num = 0
-      }
-      txtNr.innerHTML = num
-      }, 20);
   
+  document.getElementById('button').disabled = true;
+  Timeout = setInterval(() => {
+    num++
+    if(num > 9){
+      num = 0
+    }
+    txtNr.innerHTML = num
+    }, 20);
+    
 
-    setTimeout(() => {
-      test = ()=>{}
-      clearTimeout(Timeout)
-      txtNr.innerHTML = getRandomInt(lowNr,highNr)
-    }, 1000);
-}
-
-/*
-
-if(document.getElementById("button").onClick === true){
-function reset(){
   setTimeout(() => {
-  console.log('tru')
-  }, 200);
-}
+    document.getElementById('button').disabled = false;
+    test = ()=>{}
+    clearTimeout(Timeout)
+    txtNr.innerHTML = getRandomInt(lowNr,highNr)
+  }, 1000);
 }else{
-  function reset(){
-    setTimeout(() => {
-    console.log(' not tru')
-    }, 200);
-  }
-} 
-*/
+  alert('Please fill both of the numbers')
+}}
+
